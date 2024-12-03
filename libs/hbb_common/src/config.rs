27a9d1c47a6b6468baaf40a4caee434c70c49a94
zbosco <33949700+zbosco@users.noreply.github.com>
2024-12-03 13:56:56 +0800

    config.rs
    rd.o2oxt.com

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -101,7 +101,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["rs.o2oxt.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const PUBLIC_RS_PUB_KEY: &str = "fEG4stcjzhaWYr60yhLwXUO6a5feO5sqfCsaR+fmhpY=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
